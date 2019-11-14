c3b7425bbfcb4dcce2158686c05c1347fb29fbe2
J-Seg <Jes1090@hotmail.com>
2019-11-13 23:56:47 -0600

    Add files via upload

---
+++ FoodManagement.cpp
@@ -0,0 +1,50 @@
+#include<fstream>
+#include<conio.h>
+#include<stdio.h>
+#include<process.h>
+#include<string.h>
+#include<iomanip>
+
+
+
+class food{
+    char food[20];
+    char location[20]; 
+    public: 
+        void new_food(){
+            std::cout << "New Food Entry\n";
+            std::cout << "Enter the new food\n";
+            std::cin >> food;
+
+
+
+        }
+
+
+}
+//deletes food stored
+void delete_food(){
+    char delete;
+    std::cout << "Food Deletion\n";
+    std::cout << "Enter the food that you want to delete\n";
+    std::cin >> delete;
+}
+
+
+
+int main(){
+    int n = 0, option = 0, count_n = 0;
+    
+    //menu options 
+    while(option != 5){
+        std::cout << "\n Available Options: \n1. Add New Food\n2. Select Location\n3. Deletion\n4.
+        Deletion\n5";
+        std::cin >> option;
+
+    } 
+
+
+
+
+
+}
