f7059cad1c7aa82186935122e1acaa1f2ba17cb7
J-Seg <Jes1090@hotmail.com>
2019-11-14 00:19:39 -0600

    Add files via upload

--- FoodManagement.cpp
+++ FoodManagement.cpp
@@ -19,7 +19,11 @@ class food{
 
 
         }
-
+        void location(){
+            std::cout << "Location\n";
+            std::cout << "Enter where food is located\n";
+            std::cin >> location;        
+        }
 
 }
 //deletes food stored
@@ -38,10 +42,21 @@ int main(){
     //menu options 
     while(option != 5){
         std::cout << "\n Available Options: \n1. Add New Food\n2. Select Location\n3. Deletion\n4.
-        Deletion\n5";
+        Deletion\n5. Exit\n6.";
         std::cin >> option;
 
     } 
+
+    if(option == 1){
+        new_food();
+    }
+    else if(option == 2){
+        location();
+
+    }
+    else if(option == 3){
+        delete_food();
+    }
 
 
 
