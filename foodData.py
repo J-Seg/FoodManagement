8d86092967af2a00571f3218bf8e813d95d02c3a
J-Seg <Jes1090@hotmail.com>
2019-11-17 00:25:50 -0600

    Add files via upload

---
+++ foodData.py
@@ -0,0 +1,15 @@
+
+
+Food = []
+Location = []
+
+prompt = '>'
+
+print("Please enter the food you want to store: ")
+Food.append(input)
+
+print("Now please enter the location of where the food is: ")
+Location.append(input)
+
+print("Food: " + Food)
+print("Location: " + Location)
